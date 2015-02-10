69ad57c10d080e9b1ef9749adbcc94d5a74e5021
Ioannis Sintos <sintosioannis@outlook.com>
2015-02-10 15:07:44 +0100

    Added string methods to prototype

--- app/js/app.js
+++ app/js/app.js
@@ -12,3 +12,13 @@ angular.module('rsvp', [
     $routeProvider.when('/logs/:displayTrial', {templateUrl: 'partials/rsvp_trial.html', controller: 'TrialController'});
     $routeProvider.otherwise( {redirectTo: '/rsvp'} );
 }]);
+
+
+
+String.prototype.endsWith = function(suffix){
+    return this.indexOf(suffix , this.length - suffix.length) !== -1;
+}
+
+String.prototype.startsWith = function(str){
+    return this.indexOf(str) == 0;
+}
