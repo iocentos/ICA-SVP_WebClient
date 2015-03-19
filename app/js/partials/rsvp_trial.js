016d776e0daf1c986d39ff487e5b8eb0550febd8
danielgg88 <daniel.gg88@gmail.com>
2015-03-19 19:57:20 +0100

    Trial analysis ready to plot processed data. Dynamic bg added to trial analysis.

--- app/js/partials/rsvp_trial.js
+++ app/js/partials/rsvp_trial.js
@@ -1,84 +1,105 @@
+
+var TITLE_ORIGINAL = "Original data";
+var TITLE_PROCESSED = "Processed data";
+var UNIT = "mm";
+var LABEL_X = "Time(ms)";
+var LABEL_Y = "Dilation(mm)";
+var LABEL_EYE_LEFT = "Left eye";
+var LABEL_EYE_RIGHT = "Right eye";
+var LABEL_DELAY = "Delay";
+var GRAPH_CONTAINER_ORIGINAL = "original";
+var GRAPH_CONTAINER_PROCESSED = "processed";
+var PARAM_EYES_ORIGINAL = "eyes_original";
+var PARAM_EYES_PROCESSED = "eyes_processed";
+var PARAM_VALUE = "value";
+var MARKER_IMG = "url(" + defaults.marker_url + ")";
 var scp;
 
 $(document).ready(function(){
-
     $('body').css("background-color","#FFFFFF");
-
     setTimeout(function(){ 
         scp = angular.element('.main').scope(); 
         Graph(scp.trial);
+        setGradientBackground(scp.trial.configuration.app_bg , scp.trial.configuration.bg_modality);
     }, 500);
 });
 
-//Graph data
 function Graph(trial){
-
-    var img_url = "url(" + defaults.marker_url + ")";
-
-    var left_data = [];
-    var right_data = [];
+	//Graph data
+	var data_original_left = [];
+	var data_original_right = [];
+	var data_processed_left = [];
+	var data_processed_right = [];
 
     $.each(trial.data, function( index, value ){
-        
         var item = value.item;
-
         //If null, set delay as value
-        if(item.value == null){
-            item["value"] = "Delay";
-            console.log (item.value);
-        }
-
-        //Add Eye data from current item
-        $.each($(value.eyes), function( index, eyes ){
-            
-            var left_eye = eyes.left_eye;
-            var right_eye = eyes.right_eye;    
-
-            //Add item value to the first eyes data in the set
-            if(index == 0){
-                var marker = {};
-                marker['symbol'] = img_url;
-                //Left eye
-                var obj = {};
-                obj["x"] = eyes.timestamp;
-                obj["y"] = left_eye.diameter;
-                obj["name"] = item.value;
-                obj["marker"] = marker;
-                left_data.push(obj);
-                
-                //Right eye
-                var obj = {};
-                obj["x"] = eyes.timestamp;
-                obj["y"] = right_eye.diameter;
-                obj["name"] = item.value;
-                obj["marker"] = marker;
-                right_data.push(obj);
-            }else{
-                //Left eye
-                var xy = [];
-                xy[0] = eyes.timestamp;
-                xy[1] = left_eye.diameter;
-                left_data.push(xy);
-                //Right eye
-                var xy = [];
-                xy[0] = eyes.timestamp;
-                xy[1] = right_eye.diameter;
-                right_data.push(xy);
-            }
-        });
+        if(item.value == null)
+            item[PARAM_VALUE] = LABEL_DELAY;
+       	//Format original data to graph
+    	$.each($(value[PARAM_EYES_ORIGINAL]), function( index, eyes ){
+    			data_original_left.push(formatInput(item, eyes.timestamp, eyes.left_eye, index));
+    			data_original_right.push(formatInput(item, eyes.timestamp, eyes.right_eye, index));
+    	 });
+    	//Format processed data to graph
+    	$.each($(value[PARAM_EYES_PROCESSED]), function( index, eyes ){
+    			data_processed_left.push(formatInput(item, eyes.timestamp, eyes.left_eye, index));
+    			data_processed_right.push(formatInput(item, eyes.timestamp, eyes.right_eye, index));
+    	 });
     });
+	//Draw graphs
+	drawGraph(GRAPH_CONTAINER_ORIGINAL, TITLE_ORIGINAL, data_original_left, data_original_right);
+	drawGraph(GRAPH_CONTAINER_PROCESSED, TITLE_PROCESSED, data_processed_left, data_processed_right);
+}
 
-    //Show line chart
-    $('#container').highcharts({
+function setGradientBackground(color, modality){
+	//Set background color
+	var final_color;
+	if(modality == 1)
+		final_color = "#FFFFFF";
+	else if(modality == 2)
+		final_color = "#000000";
+
+	if(final_color) //Dynamic
+		$("#gradient").css("background", "linear-gradient(to right," + color + "," + final_color + ")");
+	else //Static
+		$("#gradient").css("background-color", color);
+}
+
+function formatInput(item, timestamp, eye, index){
+	//Prepare data to graph
+	var data;
+    //Add item value to the first eyes data in the set
+    if(index == 0){
+        var marker = {};
+        marker['symbol'] = MARKER_IMG;
+        var obj = {};
+        obj["x"] = timestamp;
+        obj["y"] = eye.diameter;
+        obj["name"] = item.value;
+        obj["marker"] = marker;
+        data = obj;
+    }else{
+        var xy = [];
+        xy[0] = timestamp;
+        xy[1] = eye.diameter;
+        data = xy;
+    }
+	return data;
+}
+
+function drawGraph(id, title, left_data, right_data){
+	//Draw line chart
+    $('#' + id).highcharts({
         chart:{
             type: 'spline',
             zoomType: "x"
         },
         title: {
-            text: trial.configuration.trial, x: -20 //center
+            text: title, x: -20 //center
         },
         tooltip: {
-            valueSuffix: 'mm',
+            valueSuffix: UNIT,
             crosshairs: true,
             shared: true
         },
@@ -92,7 +113,7 @@ function Graph(trial){
         },
         xAxis: {
             title: {
-                text: 'Time(ms)'
+                text: LABEL_X
             },
             labels: { 
                 rotation: 0,
@@ -103,7 +124,7 @@ function Graph(trial){
         },
         yAxis: {
             title: {
-                text: 'Dilation(mm)'
+                text: LABEL_Y
             },
             plotLines: [{
                 value: 0,
@@ -113,10 +134,10 @@ function Graph(trial){
             allowDecimals: true
         },
         series: [{
-            name: 'Left eye',
+            name: LABEL_EYE_LEFT,
             data: left_data
         }, {
-            name: 'Right eye',
+            name: LABEL_EYE_RIGHT,
             data: right_data
         }]
     });
