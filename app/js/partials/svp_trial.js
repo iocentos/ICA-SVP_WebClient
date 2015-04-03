d20990c209b5ee011d1b5050356b166b60959e57
Ioannis Sintos <sintosioannis@outlook.com>
2015-04-03 19:01:55 +0200

    Updated graphs to zoom together

--- app/js/partials/svp_trial.js
+++ app/js/partials/svp_trial.js
@@ -11,17 +11,30 @@ var GRAPH_CONTAINER_ORIGINAL = "original";
 var GRAPH_CONTAINER_PROCESSED = "processed";
 var PARAM_EYES = "eyes";
 var PARAM_VALUE = "value";
-var MARKER_IMG = "url(" + defaults.marker_url + ")";
+var MARKER_IMG = "url(" + defaults.marker_url + ")"
 var scp;
 
 $(document).ready(function(){
-    $('body').css("background-color","#FFFFFF");
-    setTimeout(function(){ 
-        scp = angular.element('.main').scope(); 
-        Graph(scp.trial);
-        setGradientBackground(scp.trial.configuration.app_bg , scp.trial.configuration.bg_modality);
-    }, 500);
-});
+
+		//original graph
+		var chart1;
+		//processed graph
+		var chart2;
+
+		$('body').css("background-color","#FFFFFF");
+		setTimeout(function(){ 
+			scp = angular.element('.main').scope(); 
+			Graph(scp.trial);
+			setGradientBackground(scp.trial.configuration.app_bg , scp.trial.configuration.bg_modality);
+		}, 500);
+
+
+		var myPlotLineId = "myPlotLine";
+		var controllingChart;
+
+		var defaultTickInterval = 5;
+		var currentTickInterval = defaultTickInterval; 
+	});
 
 function Graph(trial){
 	//Graph data
@@ -30,19 +43,19 @@ function Graph(trial){
 	var data_processed_left = [];
 	var data_processed_right = [];
 
-    $.each(trial.data, function( index, value ){
-        var item = value.item;
-        //If null, set delay as value
-        if(item.value == null)
-            item[PARAM_VALUE] = LABEL_DELAY;
-       	//Format original data to graph
-    	$.each($(value[PARAM_EYES]), function( index, eyes ){
-    			data_original_left.push(formatInput(item, eyes.timestamp, eyes.left_eye, index));
-    			data_original_right.push(formatInput(item, eyes.timestamp, eyes.right_eye, index));
-    			data_processed_left.push(formatInput(item, eyes.timestamp, eyes.left_eye_processed, index));
-    			data_processed_right.push(formatInput(item, eyes.timestamp, eyes.right_eye_processed, index));
-    	 });
-    });
+	$.each(trial.data, function( index, value ){
+		var item = value.item;
+			//If null, set delay as value
+			if(item.value == null)
+				item[PARAM_VALUE] = LABEL_DELAY;
+			//Format original data to graph
+			$.each($(value[PARAM_EYES]), function( index, eyes ){
+				data_original_left.push(formatInput(item, eyes.timestamp, eyes.left_eye, index));
+				data_original_right.push(formatInput(item, eyes.timestamp, eyes.right_eye, index));
+				data_processed_left.push(formatInput(item, eyes.timestamp, eyes.left_eye_processed, index));
+				data_processed_right.push(formatInput(item, eyes.timestamp, eyes.right_eye_processed, index));
+			});
+		});
 	//Draw graphs
 	drawGraph(GRAPH_CONTAINER_ORIGINAL, TITLE_ORIGINAL, data_original_left, data_original_right);
 	drawGraph(GRAPH_CONTAINER_PROCESSED, TITLE_PROCESSED, data_processed_left, data_processed_right);
@@ -65,72 +78,217 @@ function setGradientBackground(color, modality){
 function formatInput(item, timestamp, eye, index){
 	//Prepare data to graph
 	var obj = {};
-    //Add item value to the first eyes data in the set
-    if(index == 0){
-        var marker = {};
-        marker['symbol'] = MARKER_IMG;
-        obj["marker"] = marker;
-    }
+	//Add item value to the first eyes data in the set
+	if(index == 0){
+		var marker = {};
+		marker['symbol'] = MARKER_IMG;
+		obj["marker"] = marker;
+	}
 
-    obj["x"] = timestamp;
-    obj["y"] = eye.diameter;
-    obj["name"] = item.value + " - " + timestamp;
+	obj["x"] = timestamp;
+	obj["y"] = eye.diameter;
+	obj["name"] = item.value + " - " + timestamp;
 	return obj;
 }
 
+
+//Graphs functionality
+
 function drawGraph(id, title, left_data, right_data){
-	//Draw line chart
-    $('#' + id).highcharts({
-        chart:{
-            type: 'spline',
-            zoomType: "x"
-        },
-        title: {
-            text: title, x: -20 //center
-        },
-        tooltip: {
-            valueSuffix: UNIT,
-            crosshairs: true,
-            shared: true
-        },
-        plotOptions: {
-            series: {
-                marker: {
-                    enabled: true,
-                    radius: 0,
-                }
-            }
-        },
-        xAxis: {
-            title: {
-                text: LABEL_X
-            },
-            labels: { 
-                rotation: 0,
-                step:1,
-                staggerLines: 1,
-                overflow: "justify"
-            },
-        },
-        yAxis: {
-            title: {
-                text: LABEL_Y
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }],
-            allowDecimals: true,
-            min:0,
-            max:7
-        },
-        series: [{
-            name: LABEL_EYE_LEFT,
-            data: left_data
-        }, {
-            name: LABEL_EYE_RIGHT,
-            data: right_data
-        }]
-    });
+
+	if( id === GRAPH_CONTAINER_ORIGINAL ){
+
+		var chart1Events = {
+
+				afterSetExtremes:function(){
+
+					if (!this.chart.options.chart.isZoomed)
+					{                                         
+						var xMin = this.chart.xAxis[0].min;
+						var xMax = this.chart.xAxis[0].max;
+
+						var zmRange = computeTickInterval(xMin, xMax);
+						chart1.xAxis[0].options.tickInterval =zmRange;
+						chart1.xAxis[0].isDirty = true;
+						chart2.xAxis[0].options.tickInterval = zmRange;
+						chart2.xAxis[0].isDirty = true;
+
+						chart2.options.chart.isZoomed = true;
+
+						chart2.xAxis[0].setExtremes(xMin, xMax, true);
+						chart2.options.chart.isZoomed = false;
+
+					}
+				}
+			};
+
+		chart1 = setUpGraphToDraw(GRAPH_CONTAINER_ORIGINAL, title, left_data,right_data , chart1Events);
+	}
+	else if(id === GRAPH_CONTAINER_PROCESSED ){
+
+		var chart2Events = {
+				afterSetExtremes: function() {
+					if (!this.chart.options.chart.isZoomed) 
+					{
+						var xMin = this.chart.xAxis[0].min;
+						var xMax = this.chart.xAxis[0].max;
+						var zmRange = computeTickInterval(xMin, xMax);
+						chart1.xAxis[0].options.tickInterval =zmRange;
+						chart1.xAxis[0].isDirty = true;
+						chart2.xAxis[0].options.tickInterval = zmRange;
+						chart2.xAxis[0].isDirty = true;
+
+						chart1.options.chart.isZoomed = true;
+						chart1.xAxis[0].setExtremes(xMin, xMax, true);
+						chart1.options.chart.isZoomed = false;                                
+					}
+				}
+			};
+
+		chart2 = setUpGraphToDraw(GRAPH_CONTAINER_PROCESSED, title, left_data,right_data , chart2Events);
+	}
+
 }
+
+function setUpGraphToDraw(containerId, title, left_data , right_data, events){
+	var chart = new Highcharts.Chart({
+		chart:{
+			renderTo: containerId,
+			type: 'spline',
+			zoomType: "x"
+		},
+		title: {
+		text: title, x: -20 //center
+		},
+		tooltip: {
+			valueSuffix: UNIT,
+			crosshairs: true,
+			shared: true
+		},
+		plotOptions: {
+			series: {
+				marker: {
+					enabled: true,
+					radius: 0,
+				}
+			}
+		},
+		xAxis: {
+			title: {
+				text: LABEL_X
+			},
+			labels: { 
+				rotation: 0,
+				step:1,
+				staggerLines: 1,
+				overflow: "justify"
+			},
+			events:events
+		},
+		yAxis: {
+			title: {
+				text: LABEL_Y
+			},
+			plotLines: [{
+				value: 0,
+				width: 1,
+				color: '#808080'
+			}],
+			allowDecimals: true,
+			min:0,
+			max:7
+		},
+		series: [{
+			name: LABEL_EYE_LEFT,
+			data: left_data
+		}, {
+			name: LABEL_EYE_RIGHT,
+			data: right_data
+		}],
+			}, function(chart) { //add this function to the chart definition to get synchronized crosshairs
+				syncronizeCrossHairs(chart);
+
+			});
+
+
+	return chart;
+}
+
+function syncronizeCrossHairs(chart) {
+	var container = $(chart.container),
+	offset = container.offset(),
+	x, y, isInside, report;
+
+	container.mousemove(function(evt) {
+
+		x = evt.clientX - chart.plotLeft - offset.left;
+		y = evt.clientY - chart.plotTop - offset.top;
+		var xAxis = chart.xAxis[0];
+			//remove old plot line and draw new plot line (crosshair) for this chart
+			var xAxis1 = chart1.xAxis[0];
+			xAxis1.removePlotLine("myPlotLineId");
+			xAxis1.addPlotLine({
+				value: chart.xAxis[0].translate(x, true),
+				width: 1,
+				color: 'red',
+			//dashStyle: 'dash',                   
+			id: "myPlotLineId"
+			});
+						//remove old crosshair and draw new crosshair on chart2
+						var xAxis2 = chart2.xAxis[0];
+						xAxis2.removePlotLine("myPlotLineId");
+						xAxis2.addPlotLine({
+							value: chart.xAxis[0].translate(x, true),
+							width: 1,
+							color: 'red',
+			//dashStyle: 'dash',                   
+			id: "myPlotLineId"
+			});
+
+			//if you have other charts that need to be syncronized - update their crosshair (plot line) in the same way in this function.                   
+	});
+}
+
+//compute a reasonable tick interval given the zoom range -
+//have to compute this since we set the tickIntervals in order
+//to get predictable synchronization between 3 charts with
+//different data.
+function computeTickInterval(xMin, xMax) {
+	var zoomRange = xMax - xMin;
+
+	if (zoomRange <= 2)
+		currentTickInterval = 0.5;
+	if (zoomRange < 20)
+		currentTickInterval = 1;
+	else if (zoomRange < 100)
+		currentTickInterval = 5;
+}
+
+//explicitly set the tickInterval for the 3 charts - based on
+//selected range
+function setTickInterval(event) {
+	var xMin = event.xAxis[0].min;
+	var xMax = event.xAxis[0].max;
+	computeTickInterval(xMin, xMax);
+
+	chart1.xAxis[0].options.tickInterval = currentTickInterval;
+	chart1.xAxis[0].isDirty = true;
+	chart2.xAxis[0].options.tickInterval = currentTickInterval;
+	chart2.xAxis[0].isDirty = true;
+
+}
+
+//reset the extremes and the tickInterval to default values
+function unzoom() {
+	chart1.xAxis[0].options.tickInterval = defaultTickInterval;
+	chart1.xAxis[0].isDirty = true;
+	chart2.xAxis[0].options.tickInterval = defaultTickInterval;
+	chart2.xAxis[0].isDirty = true;
+
+	chart1.xAxis[0].setExtremes(null, null);
+	chart2.xAxis[0].setExtremes(null, null);
+}
+
+$('#zoom_button').click(function(){
+	unzoom();
+});
